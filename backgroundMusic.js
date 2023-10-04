ebcd3fd22f84fa5a5673878957b9e52575288068
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2023-10-04 09:57:43 -0400

    Create backgroundMusic.js

---
+++ backgroundMusic.js
@@ -0,0 +1,4 @@
+var audio = new Audio("music/song1.mp3");
+audio.volume = 0.15; // Set the volume to 15% of the original volume
+audio.loop = true; // Enable looping
+audio.autoplay = true; // Start playing automatically
