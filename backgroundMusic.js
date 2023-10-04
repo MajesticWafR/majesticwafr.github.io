cf9e70a66200d72980088d0011ab9789d001ee6c
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2023-10-04 10:17:16 -0400

    Delete backgroundMusic.js

--- backgroundMusic.js
+++
@@ -1,4 +0,0 @@
-var audio = new Audio("music/song1.mp3");
-audio.volume = 0.15; // Set the volume to 15% of the original volume
-audio.loop = true; // Enable looping
-audio.autoplay = true; // Start playing automatically
