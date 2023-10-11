72b57825798cc0e652c9400e754c6cdf6499df14
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2023-10-11 11:32:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Get the user's IP address
 const userIP = '192.149.67.147'; // Replace with your desired IP address
 
-// Function to close the tab
-function closeTab() {
-  window.close(); // Close the current tab
+// Function to display an alert message
+function showAlert() {
+  alert('Access denied. You are not allowed to access this website.'); // Display the alert message
 }
 
 // Check if the user's IP matches the specified IP address
 if (userIP === window.location.hostname) {
-  closeTab(); // Close the tab if the IP matches
+  showAlert(); // Display the alert if the IP matches
 }
